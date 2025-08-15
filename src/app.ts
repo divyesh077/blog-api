b01fca2587674e5cac57a6ca9d4187c9857ef59c
divyesh <divyesh.i.r75@gmail.com>
2025-08-15 16:42:19 +0530

    add security and parser middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import express from "express";
 import v1Router from "./routes/v1";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+import { limiter } from "./middleware/rateLimit";
 
-//Init the application.
+// Init the application.
 const app = express();
 
-//API Routing for v1
+// Security
+app.use(helmet());
+app.use(cors());
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
+
+// Logger
+app.use(morgan("dev"));
+
+// Parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API Routing for v1
 app.use("/api/v1", v1Router);
 
 export { app };
