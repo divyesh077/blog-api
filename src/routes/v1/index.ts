a13d85f2a0f3b496e8bcf39e3bc8eba17603be91
divyesh <divyesh.i.r75@gmail.com>
2025-08-29 21:58:03 +0530

    create and add auth routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import healthRouter from "./health";
 import userRouter from "./user.routes";
+import authRouter from "./auth.routes";
 
 const router = express.Router();
 
 router.use("/health", healthRouter);
 
+router.use("/auth", authRouter);
 router.use("/users", userRouter);
 
 export default router;
